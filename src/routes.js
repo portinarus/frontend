c8fb85ef31f6cbea1bb13b64bcef6f9831f2070d
Victor Pantoja <victor.pantoja@gmail.com>
2018-03-03 19:42:16 +0100

    Basic HTML for index, login and user home

--- src/routes.js
+++ src/routes.js
@@ -4,12 +4,14 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import Layout from './components/Layout';
 import IndexPage from './components/IndexPage';
+import HomeUserPage from './components/HomeUser';
 import PhotoPage from './components/PhotoPage';
 import NotFoundPage from './components/NotFoundPage';
 
 const routes = (
   <Route path="/" component={Layout}>
     <IndexRoute component={IndexPage}/>
+    <Route path="user/:id" component={HomeUserPage}/>
     <Route path="photo/:id" component={PhotoPage}/>
     <Route path="*" component={NotFoundPage}/>
   </Route>
